9d1b76b6d045cbb866fc1f321cb069e81651fb84
Jason Pilz <jasonpilz@gmail.com>
2017-02-23 22:18:31 -0800

    Initial commit

---
+++ lib/pwnr.ex
@@ -0,0 +1,18 @@
+defmodule Pwnr do
+  @moduledoc """
+  Documentation for Pwnr.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Pwnr.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/pwnr_test.exs
@@ -0,0 +1,8 @@
+defmodule PwnrTest do
+  use ExUnit.Case
+  doctest Pwnr
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
