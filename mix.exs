f69ea8ad0edd5d8c9145828a66ff37f006cdb424
Jason Pilz <jasonpilz@gmail.com>
2017-02-25 18:20:20 -0800

    Add httpoison, timex and tzdata to mix

--- mix.exs
+++ mix.exs
@@ -10,24 +10,22 @@ defmodule Pwnr.Mixfile do
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger]]
+    [
+      extra_applications: [
+        :logger,
+        :httpoison,
+        :timex,
+        :tzdata
+      ]
+    ]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:httpoison, "~> 0.11.0"},
+      {:timex, "~> 3.1"},
+      {:tzdata, "~> 0.5.10"}
+    ]
   end
 end
