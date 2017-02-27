755d151e9c911ecf49bff39c682253e2815e320f
Jason Pilz <jasonpilz@gmail.com>
2017-02-26 18:33:13 -0800

    Complete un-distributed version

--- lib/pwnr.ex
+++ lib/pwnr.ex
@@ -10,7 +10,46 @@ defmodule Pwnr do
     worker_fun = fn -> Worker.start(url) end
 
     1..n_workers
-    |> Enum.map(fn _ -> Task.async(worker_fun) end)
-    |> Enum.map(&Task.await(&1))
+      |> Enum.map(fn _ -> Task.async(worker_fun) end)
+      |> Enum.map(&Task.await(&1, :infinity))
+      |> parse_results
+  end
+
+  defp parse_results(results) do
+    {successes, _failures} =
+      results
+        |> Enum.partition(fn x ->
+             case x do
+               {:ok, _} -> true
+               _        -> false
+           end
+        end)
+
+    total_workers = Enum.count(results)
+    total_success = Enum.count(successes)
+    total_failure = total_workers - total_success
+
+    data = successes |> Enum.map(fn {:ok, time} -> time end)
+    ave_time = average(data)
+    min_time = Enum.min(data)
+    max_time = Enum.max(data)
+
+    IO.puts """
+    Total workers    : #{total_workers}
+    Successful reqs  : #{total_success}
+    Failed reqs      : #{total_failure}
+    Average (msecs)  : #{ave_time}
+    Longest (msecs)  : #{max_time}
+    Shortest (msecs) : #{min_time}
+    """
+  end
+
+  defp average(list) do
+    sum = Enum.sum(list)
+    if sum > 0 do
+      sum / Enum.count(list)
+    else
+      0
+    end
   end
 end
